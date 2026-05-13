98bdace4c93ca89e2a2df264cdadf7ddb2dc8ef2
김석환 <ndo05115@gmail.com>
2026-05-13 11:03:58 +0900

    소문자 컨트롤러 삭제하며 사라진 컨트롤러들 대문자 컨트롤러에 추가

---
+++ edusys/src/main/java/koreanit/lms/edusys/Controller/WorkController.java
@@ -0,0 +1,36 @@
+package koreanit.lms.edusys.Controller;
+
+import koreanit.lms.edusys.Entity.Work;
+import koreanit.lms.edusys.Service.WorkService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/works")
+@RequiredArgsConstructor
+public class WorkController {
+
+    private final WorkService workService;
+
+    @GetMapping
+    public List<Work> getAllWorks() {
+        return workService.findAllWorks();
+    }
+
+    @GetMapping("/{id}")
+    public Work getWorkById(@PathVariable Integer id) {
+        return workService.findWorkById(id)
+                .orElseThrow(() -> new RuntimeException("Work not found with id: " + id));
+    }
+
+    @PostMapping
+    public Work saveWork(@RequestBody Work work) {
+        return workService.createWork(work);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteWork(@PathVariable Integer id) {
+        workService.deleteWork(id);
+    }
+}
