da604feef3ad1362b464be53b4b26243433ebf2d
김석환 <ndo05115@gmail.com>
2026-05-13 11:09:18 +0900

    사라진 GradeController 추가, 메인 페이지에 교사용/관리자용 임시 추가

---
+++ edusys/src/main/java/koreanit/lms/edusys/Controller/GradeController.java
@@ -0,0 +1,36 @@
+package koreanit.lms.edusys.Controller;
+
+import koreanit.lms.edusys.Entity.Grade;
+import koreanit.lms.edusys.Service.GradeDTO;
+import koreanit.lms.edusys.Service.GradeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/grades")
+@RequiredArgsConstructor
+public class GradeController {
+
+    private final GradeService gradeService;
+
+    @GetMapping
+    public List<GradeDTO> getAllGrades() {
+        return gradeService.findAllGrades();
+    }
+
+    @GetMapping("/{id}")
+    public GradeDTO getGradeById(@PathVariable Integer id) {
+        return gradeService.findGradeById(id);
+    }
+
+    @PostMapping
+    public Grade saveGrade(@RequestBody Grade grade) {
+        return gradeService.createGrade(grade);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteGrade(@PathVariable Integer id) {
+        gradeService.deleteGrade(id);
+    }
+}
