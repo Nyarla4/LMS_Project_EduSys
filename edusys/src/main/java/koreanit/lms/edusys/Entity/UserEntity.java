4f3dfc0720d1e082784d94b15a7a2e3578c444d1
김석환 <ndo05115@gmail.com>
2026-05-14 11:23:25 +0900

    교사처리 누락된 부분 수정

--- edusys/src/main/java/koreanit/lms/edusys/Entity/UserEntity.java
+++ edusys/src/main/java/koreanit/lms/edusys/Entity/UserEntity.java
@@ -36,6 +36,6 @@ public class UserEntity {
 
     @Enumerated(EnumType.STRING)
     @Column(name = "user_type", nullable = false, length = 1, columnDefinition = "CHAR(1) DEFAULT 'S'") 
-    private UserType usertype = UserType.S; 
+    private UserType usertype; 
     
 }
